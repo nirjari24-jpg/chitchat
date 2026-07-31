e6cfad1dc2bc316fe13c1c8232e645f28704462f
nirjari24-jpg <nirjari24@gmail.com>
2026-07-31 21:19:15 +0530

    Bind server to 0.0.0.0 for deployment

--- server.js
+++ server.js
@@ -127,6 +127,6 @@ app.get('/profile', (req, res) => {
 
 // Start the Server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
